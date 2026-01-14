f8632a5d9cce35dfa8e44091c938f9358dda1d52
jihad <gehadsaed199@gmail.com>
2026-01-14 14:11:59 +0200

    fix: update API URLs to HTTPS for production

--- src/constants/apiConfig.js
+++ src/constants/apiConfig.js
@@ -1,5 +1,5 @@
 export const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://196.219.221.202/callcenterapi";
+  import.meta.env.VITE_API_BASE_URL || "https://goeic.stadiaholding.com";
 export const COMPLAINT_API_URL =
   import.meta.env.VITE_COMPLAINT_API_URL ||
   "http://196.219.221.202/callcenterapi/api";
