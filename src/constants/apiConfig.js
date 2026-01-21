7e64c2248359b4423f4efacd18db0083d8fccaf6
jihad <gehadsaed199@gmail.com>
2026-01-21 18:16:19 +0200

    feat: Add API base and complaint URL constants.

--- src/constants/apiConfig.js
+++ src/constants/apiConfig.js
@@ -2,4 +2,4 @@ export const API_BASE_URL =
   import.meta.env.VITE_API_BASE_URL || "https://goeic.stadiaholding.com";
 export const COMPLAINT_API_URL =
   import.meta.env.VITE_COMPLAINT_API_URL ||
-  "http://196.219.221.203/callcenterapi/api/vwAPIAll";
+  "https://196.219.221.203/callcenterapi/api/vwAPIAll";
